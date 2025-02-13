3947d3818cdb623af30c3a323bf24ab508d1e0b9
Mohamed <mohamedachek5@gmail.com>
2025-02-13 16:48:33 +0100

    Updates

---
+++ precache-manifest.d8441e1bae1702df3e9a556089f0e1c0.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69d3b005b933a70fb1430dc44fb9df81",
+    "url": "/mohamed-achek/mohamed-achek.github.io.git/index.html"
+  },
+  {
+    "revision": "0112cf19540a63a2cf39",
+    "url": "/mohamed-achek/mohamed-achek.github.io.git/static/css/main.eb4ce1c0.chunk.css"
+  },
+  {
+    "revision": "46d382d0768ae6b09010",
+    "url": "/mohamed-achek/mohamed-achek.github.io.git/static/js/2.5a597504.chunk.js"
+  },
+  {
+    "revision": "0112cf19540a63a2cf39",
+    "url": "/mohamed-achek/mohamed-achek.github.io.git/static/js/main.8f28f907.chunk.js"
+  },
+  {
+    "revision": "da4918d5613938a2ca4e",
+    "url": "/mohamed-achek/mohamed-achek.github.io.git/static/js/runtime-main.7f86beba.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/mohamed-achek/mohamed-achek.github.io.git/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "b8349593342654d8ca5686e6cb73f13b",
+    "url": "/mohamed-achek/mohamed-achek.github.io.git/static/media/Code.b8349593.ttf"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/mohamed-achek/mohamed-achek.github.io.git/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/mohamed-achek/mohamed-achek.github.io.git/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/mohamed-achek/mohamed-achek.github.io.git/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/mohamed-achek/mohamed-achek.github.io.git/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/mohamed-achek/mohamed-achek.github.io.git/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/mohamed-achek/mohamed-achek.github.io.git/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "26d440f8baa26a75a202f59d922727e8",
+    "url": "/mohamed-achek/mohamed-achek.github.io.git/static/media/IBM-01.26d440f8.png"
+  },
+  {
+    "revision": "f517fff0e1e53ad2abf7078e6f9746bd",
+    "url": "/mohamed-achek/mohamed-achek.github.io.git/static/media/IBM-02.f517fff0.png"
+  },
+  {
+    "revision": "fe268165dc02a3d0c1b9af79d0aa30be",
+    "url": "/mohamed-achek/mohamed-achek.github.io.git/static/media/IBM-03.fe268165.png"
+  },
+  {
+    "revision": "961af905e49df1a8c08cda1a48e309ab",
+    "url": "/mohamed-achek/mohamed-achek.github.io.git/static/media/IBM-04.961af905.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/mohamed-achek/mohamed-achek.github.io.git/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/mohamed-achek/mohamed-achek.github.io.git/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/mohamed-achek/mohamed-achek.github.io.git/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "9d204f50ec8e5bcc5dcda0132d404b48",
+    "url": "/mohamed-achek/mohamed-achek.github.io.git/static/media/fa-brands-400.9d204f50.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/mohamed-achek/mohamed-achek.github.io.git/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/mohamed-achek/mohamed-achek.github.io.git/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/mohamed-achek/mohamed-achek.github.io.git/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "9443c1e7fdf95d739af3f0901224f90a",
+    "url": "/mohamed-achek/mohamed-achek.github.io.git/static/media/fa-regular-400.9443c1e7.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/mohamed-achek/mohamed-achek.github.io.git/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/mohamed-achek/mohamed-achek.github.io.git/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/mohamed-achek/mohamed-achek.github.io.git/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/mohamed-achek/mohamed-achek.github.io.git/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/mohamed-achek/mohamed-achek.github.io.git/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "3d102342391af184d5ae9e7708d8220f",
+    "url": "/mohamed-achek/mohamed-achek.github.io.git/static/media/fa-solid-900.3d102342.svg"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/mohamed-achek/mohamed-achek.github.io.git/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/mohamed-achek/mohamed-achek.github.io.git/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "bfd2399e2521bde01ea5fdd434d743ec",
+    "url": "/mohamed-achek/mohamed-achek.github.io.git/static/media/logo-01.bfd2399e.png"
+  },
+  {
+    "revision": "eacb2a58d0212722524e3b1ae5bd23f4",
+    "url": "/mohamed-achek/mohamed-achek.github.io.git/static/media/pdp.eacb2a58.png"
+  },
+  {
+    "revision": "535eb6d22b7eadad30bc0f7d90109023",
+    "url": "/mohamed-achek/mohamed-achek.github.io.git/static/media/project-01.535eb6d2.png"
+  },
+  {
+    "revision": "c4581df806f6ad42263bc79a37883696",
+    "url": "/mohamed-achek/mohamed-achek.github.io.git/static/media/project-02.c4581df8.gif"
+  },
+  {
+    "revision": "4ed2b38be66adb64de02f28c757015ab",
+    "url": "/mohamed-achek/mohamed-achek.github.io.git/static/media/project-03.4ed2b38b.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.505c75c198ed7c619710902b79244348.js"
+  "/mohamed-achek/mohamed-achek.github.io.git/precache-manifest.d8441e1bae1702df3e9a556089f0e1c0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mohamed-achek/mohamed-achek.github.io.git/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
